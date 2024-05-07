e1be664dcda14bb484b8365c5407d8e93cda3ceb
AmitGaikar00 <amitgaikar6025@gmail.com>
2024-05-07 15:29:57 +0530

    java installed

---
+++ prerequisites.sh
@@ -0,0 +1,9 @@
+yum install epel-release -y
+
+yum install ansible -y
+
+yum groupinstall 'Development Tools' -y
+
+yum install pcre pcre-devel openssl openssl-devel bzip2 bzip2-devel libffi libffi-devel zlib zlib-devel -y
+
+
