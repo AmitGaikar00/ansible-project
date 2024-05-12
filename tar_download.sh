2db7871a47f2458c6a5204d47f7e61c0b140dfad
AmitGaikar00 <amitgaikar6025@gmail.com>
2024-05-12 11:03:05 +0530

    script to install tar files

---
+++ tar_download.sh
@@ -0,0 +1,24 @@
+wget https://ftp.postgresql.org/pub/source/v16.0/postgresql-16.0.tar.bz2
+
+wget https://go.neo4j.com/download-thanks.html?edition=community&release=5.19.0&flavour=unix&_gl=1*lcszpl*_ga*MTU5MDUxNjQ5LjE3MTAzMDYzMzI.*_ga_DL38Q8KGQC*MTcxNTQ5MDIzMS4xMS4xLjE3MTU0OTAzMDUuMC4wLjA.
+
+wget https://nginx.org/download/nginx-1.26.0.tar.gz
+
+wget https://download.oracle.com/java/17/archive/jdk-17.0.10_linux-x64_bin.tar.gz
+
+wget https://www.python.org/ftp/python/3.6.8/Python-3.6.8.tgz
+wget https://www.python.org/ftp/python/3.9.9/Python-3.9.9.tgz
+wget https://www.python.org/ftp/python/3.12.1/Python-3.12.1.tgz 
+
+
+wget https://files.pythonhosted.org/packages/d5/f7/2fdd9205a2eedee7d9b0abbf15944a1151eb943001dbdc5233b1d1cfc34e/Cython-3.0.10.tar.gz
+
+wget https://files.pythonhosted.org/packages/65/6e/09db70a523a96d25e115e71cc56a6f9031e7b8cd166c1ac8438307c14058/numpy-1.26.4.tar.gz
+
+wget https://files.pythonhosted.org/packages/88/d9/ecf715f34c73ccb1d8ceb82fc01cd1028a65a5f6dbc57bfa6ea155119058/pandas-2.2.2.tar.gz
+
+wget https://files.pythonhosted.org/packages/3c/2b/3540b4af4f0a1593ee09fa20460d0f402b52aa76cf683f995555b7f64abd/pmdarima-2.0.4.tar.gz
+
+wget https://files.pythonhosted.org/packages/92/75/ce35194d8e3022203cca0d2f896dbb88689f9b3fce8e9f9cff942913519d/nltk-3.5.zip
+
+
